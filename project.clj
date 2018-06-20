5b500db5462188a486432e807233993e76011872
penland365 <Jeffrey.N.Davis@gmail.com>
2018-06-20 10:07:44 -0500

    lein new datadog-clj

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject datadog-clj "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "https://github.com/novolabs/datadog-clj"
+  :license {:name "BSD-3-Clause"
+            :url "https://opensource.org/licenses/BSD-3-Clause"}
+  :dependencies [[org.clojure/clojure "1.9.0"]])
---
+++ test/datadog_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns datadog-clj.core-test
+  (:require [clojure.test :refer :all]
+            [datadog-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
